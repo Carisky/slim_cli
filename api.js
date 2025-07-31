b485e0269db2495b9834896a9021982d1ecd776d
Yehor <158765585+Carisky@users.noreply.github.com>
2025-07-31 09:03:05 +0200

    feat: add interactive configuration

--- api.js
+++ api.js
@@ -1,31 +1,35 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
-import os from 'os';
-import path from 'path';
 import chalk from 'chalk';
+import { getConfig } from './config.js';
 
-dotenv.config({ path: path.join(os.homedir(), '.slim-cli', '.env') });
-const api = axios.create({
-  baseURL: process.env.BASE_URL,
-  headers: { 'X-API-Key': process.env.X_API_KEY }
-});
+const createApi = () => {
+  const { BASE_URL, X_API_KEY } = getConfig();
+  return axios.create({
+    baseURL: BASE_URL,
+    headers: { 'X-API-Key': X_API_KEY }
+  });
+};
 
 export const apiGet = async url => {
+  const api = createApi();
   console.log(chalk.green('GET'), url);
   return (await api.get(url)).data;
 };
 
 export const apiPost = async (url, data) => {
+  const api = createApi();
   console.log(chalk.keyword('orange')('POST'), url);
   return (await api.post(url, data)).data;
 };
 
 export const apiPut = async (url, data) => {
+  const api = createApi();
   console.log(chalk.yellow('PUT'), url);
   return (await api.put(url, data)).data;
 };
 
 export const apiDelete = async url => {
+  const api = createApi();
   console.log(chalk.red('DELETE'), url);
   return (await api.delete(url)).data;
 };
